2b39306f357a95c59fb01b35ff5e08b1e21efc60
ssoftech <project@sehrishsoftech.com>
2016-04-23 05:32:15 +0530

    Adding getter and setter

--- src/edu/gitdemo/domain/Bank.java
+++ src/edu/gitdemo/domain/Bank.java
@@ -4,5 +4,20 @@ public class Bank {
 	
 	int id;
 	String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
